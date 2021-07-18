aef04f211f1bef7dab0083655f66396cb172331a
Jaskaran Singh <jassiiit624@gmail.com>
2021-07-18 22:03:38 +0530

    Update todos.component.ts

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,12 @@
 import { DataService } from './../shared/data.service';
-
-
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../shared/todo.model';
-import { Data } from '@angular/router';
-import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
+
 export class TodosComponent implements OnInit {
 
   todos:any[]=[];
@@ -31,6 +28,7 @@ export class TodosComponent implements OnInit {
 
   onFilterTodos(): void{
     // filterDate, filterStatus as Strings
+    console.log(this.filterDate, this.filterStatus)
     this.todos=this.dataService.getFilteredTodos(this.filterDate, this.filterStatus).records;
   }
 
